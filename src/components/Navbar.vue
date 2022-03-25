5c57598b604d12f460085ee6e7504a2ea68d438a
Gustavo <gustavosz1224@gmail.com>
2022-03-25 16:40:28 -0600

    Home Page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app-bar app dark>
+    <v-app-bar-title class="mx-3">Logo</v-app-bar-title>
+    <v-container fluid>
+      <v-btn to="/" retain-focus-on-click plain class="pa-2 text-capitalize">
+        Home
+      </v-btn>
+      <v-btn to="/documents" plain class="pa-2 text-capitalize">
+        Documents
+      </v-btn>
+      <v-btn to="/files" plain class="pa-2 text-capitalize"> Files </v-btn>
+      <v-btn to="/dictionary" plain class="pa-2 text-capitalize" disabled>
+        Dictionary
+      </v-btn>
+      <v-menu open-on-hover offset-y rounded="0">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn plain class="pa-2 text-capitalize" v-on="on" v-bind="attrs">
+            Upload File
+            <v-icon dark>mdi-menu-down</v-icon>
+          </v-btn>
+        </template>
+        <v-list dense dark>
+          <v-list-item-group>
+            <v-list-item to="/new/document" exact>
+              <v-list-item-title>Upload Documents</v-list-item-title>
+            </v-list-item>
+            <v-list-item to="/new/translation" exact>
+              <v-list-item-title>Upload Translations</v-list-item-title>
+            </v-list-item>
+            <v-list-item to="/new/variant" disabled exact>
+              <v-list-item-title>Upload Variants</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <h1 class="text-h3 text-center mt-4 mb-10">Home Page</h1>
+
+    <v-row>
+      <v-col v-for="(item, i) in views" :key="i" cols="4">
+        <v-card outlined hover class="fill-height" :to="{ name: item.view }">
+          <div class="d-flex flex-no-wrap fill-height">
+            <v-avatar class="align-self-center mx-3" size="100" tile>
+              <v-img :src="item.src" :alt="item.alt"></v-img>
+            </v-avatar>
+            <div>
+              <v-card-title class="text-h5" v-text="item.title"></v-card-title>
+              <v-card-text v-text="item.text"></v-card-text>
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data: () => ({
+    views: [
+      {
+        title: "Documents",
+        text: "See the list of original documents previously uploaded to the application.",
+        src: require("../assets/documents.png"),
+        alt: "documents.png",
+        view: "Documents",
+      },
+      {
+        title: "Upload Documents",
+        text: "Upload a new document to the application to process it.",
+        src: require("../assets/upload_documents.png"),
+        alt: "upload documents.png",
+        view: "UploadDocument",
+      },
+      {
+        title: "Files",
+        text: "See all documents, translations and variants in the web application.",
+        src: require("../assets/files.png"),
+        alt: "ver archivos.png",
+        view: "Files",
+      },
+      {
+        title: "Upload Translation",
+        text: "Upload a translation of a document previously uploaded in the application.",
+        src: require("../assets/translator.png"),
+        alt: "traduccion.png",
+        view: "UploadTranslation",
+      },
+      {
+        title: "Upload Variant",
+        text: "Upload an edition or variant of a previously uploaded document.",
+        src: require("../assets/variants.png"),
+        alt: "Variante.png",
+        view: "Home",
+      },
+      {
+        title: "Dictionary",
+        text: "Consult words and lexemes to know their meaning.",
+        src: require("../assets/dictionary.png"),
+        alt: "diccionario.png",
+        view: "Home",
+      },
+    ],
+  }),
+};
 </script>
